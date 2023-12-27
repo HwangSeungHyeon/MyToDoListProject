fc7bbd4b879822e6e8e39c482ee7c6423dd8a843
HwangSeungHyeon <xlddy02@gmail.com>
2023-12-27 14:58:51 +0900

    ADD: "add DTO files"
    API 명세서를 작성하기 위해서 TodoResponse.kt, CreateTodoResponse.kt, UpdateTodoResponse.kt 파일 생성

---
+++ src/main/kotlin/com/teamsparta/mytodolist/domain/todo/dto/CreateTodoResponse.kt
@@ -0,0 +1,11 @@
+package com.teamsparta.mytodolist.domain.todo.dto
+
+import java.util.Date
+
+// 할일 카드를 작성했을 때 사용하는 DTO
+data class CreateTodoResponse(
+    val title: String, //할일 제목을 나타내는 변수
+    val description: String, //할일 내용을 나타내는 변수
+    val date: Date, //작성일을 나타내는 변수
+    val name: String //작성자 이름을 저장할 변수
+)
