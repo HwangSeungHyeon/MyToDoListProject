99896b45b040c71f01707dafd4f34c7d4ba90c36
HwangSeungHyeon <xlddy02@gmail.com>
2023-12-28 01:27:12 +0900

    COMMENT: "add comments"
    코드 설명을 위한 주석 추가

--- src/main/kotlin/com/teamsparta/mytodolist/domain/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/teamsparta/mytodolist/domain/exception/GlobalExceptionHandler.kt
@@ -4,9 +4,15 @@ import com.teamsparta.mytodolist.domain.exception.dto.ErrorResponseDto
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
+/*
+* Spring의 Web Layer의 일부
+* 하위 Layer에서 발생한 예외들을 처리하여 적절한 응답(Response)을 주는 역할
+*/
 @RestControllerAdvice
 class GlobalExceptionHandler {
     @ExceptionHandler(ModelNotFoundException::class)
+    //ModelNotFoundException: 하위 layer에서 id로 검색에 실패했을 때 발생하는 에러
+    //에러가 발생하면 메시지를 DTO에 응답(response)한다.
     fun handleModelNotFoundException(e: ModelNotFoundException): ErrorResponseDto{
         return ErrorResponseDto(e.message)
     }
--- src/main/kotlin/com/teamsparta/mytodolist/domain/exception/ModelNotFoundException.kt
+++ src/main/kotlin/com/teamsparta/mytodolist/domain/exception/ModelNotFoundException.kt
@@ -1,5 +1,6 @@
 package com.teamsparta.mytodolist.domain.exception
 
+//DB에서 찾는 id가 없을 때 에러 메시지를 반환하는 클래스
 data class ModelNotFoundException(
     private val modelName: String,
     private val id: Long
