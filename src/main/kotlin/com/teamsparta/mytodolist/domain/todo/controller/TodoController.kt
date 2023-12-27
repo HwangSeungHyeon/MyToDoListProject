9ef4b0725218632e6172f60e16554528fb0e111b
HwangSeungHyeon <xlddy02@gmail.com>
2023-12-27 16:06:16 +0900

    Comment: "주석 맞춤법 수정"
    할일 -> 할 일로 수정

--- src/main/kotlin/com/teamsparta/mytodolist/domain/todo/controller/TodoController.kt
+++ src/main/kotlin/com/teamsparta/mytodolist/domain/todo/controller/TodoController.kt
@@ -10,35 +10,35 @@ import org.springframework.web.bind.annotation.*
 @RestController //TodoController를 Bean으로 등록
 class TodoController {
     @GetMapping() //GET 메소드 핸들링, /todos에 접근한다
-    //할일 목록 리스트를 가져오는 메소드
+    //할 일 목록 리스트를 가져오는 메소드
     //Entity를 반환(Repository에 접근해야 하니까)
     fun getTodoList(): ResponseEntity<List<TodoResponse>> {
         TODO("not implemented") //아직 기능 구현 안 함
     }
 
     @GetMapping("/{todoId}") //GET 메소드 핸들링, /todos/{todoId}에 접근한다
-    //선택한 할일 카드를 가져오는 메소드
+    //선택한 할 일 카드를 가져오는 메소드
     //todoId를 argument로 받아서 Entity를 반환(Repository에 접근해야 하니까)
     fun getTodo(@PathVariable todoId: Long): ResponseEntity<TodoResponse>{
         TODO("not implemented") //아직 기능 구현 안 함
     }
 
     @PostMapping() //POST 메소드 핸들링, /todos에 접근한다
-    //할일 카드를 만드는 메소드
+    //할 일 카드를 만드는 메소드
     //CreateTodoResponse DTO를 argument로 받아서 Entity를 반환(Repository에 접근해야 하니까)
     fun createTodo(@RequestBody createTodoResponse: CreateTodoResponse): ResponseEntity<CreateTodoResponse>{
         TODO("not implemented") //아직 기능 구현 안 함
     }
 
     @PutMapping("/{todoId}") //GET 메소드 핸들링, /todos/todoId에 접근한다
-    //선택한 할일 카드를 수정하는 메소드
+    //선택한 할 일 카드를 수정하는 메소드
     //todoId와 UpdateTodoResponsef DTO를 argument로 받아서 Entity를 반환(Repository에 접근해야 하니까)
     fun updateTodo(@PathVariable todoId: Long, @RequestBody updateTodoResponse: UpdateTodoResponse): ResponseEntity<UpdateTodoResponse>{
         TODO("not implemented") //아직 기능 구현 안 함
     }
 
     @DeleteMapping("/{todoId}") //GET 메소드 핸들링, /todos/todoId에 접근한다
-    //선택한 할일을 삭제하는 메소드
+    //선택한 할 일을 삭제하는 메소드
     fun deleteTodo(@PathVariable todoId: Long){
         TODO("not implemented") //아직 기능 구현 안 함
     }
--- src/main/kotlin/com/teamsparta/mytodolist/domain/todo/dto/UpdateTodoResponse.kt
+++ src/main/kotlin/com/teamsparta/mytodolist/domain/todo/dto/UpdateTodoResponse.kt
@@ -1,8 +1,8 @@
 package com.teamsparta.mytodolist.domain.todo.dto
 
-// 할일 카드를 수정했을 때 사용하는 DTO
+// 할 일 카드를 수정했을 때 사용하는 DTO
 data class UpdateTodoResponse(
-    val title: String, //할일 제목을 나타내는 변수
-    val description: String, //할일 내용을 나타내는 변수
+    val title: String, //할 일 제목을 나타내는 변수
+    val description: String, //할 일 내용을 나타내는 변수
     val name: String //작성자 이름을 저장할 변수
 )
